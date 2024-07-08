363b334ade3cecaca70e93acf8e5362b7f9dbaa6
Renato Santos <devrenato.js@gmail.com>
2024-07-08 00:20:02 -0300

    render: added postgres config online

--- src/main/java/com/msleads/msleads/service/DatabaseConnectionService.java
+++ src/main/java/com/msleads/msleads/service/DatabaseConnectionService.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnectionService {
-    private static final String DB_URL = "jdbc:postgresql://localhost:5433/postgres";
-    private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "root";
+    private static final String DB_URL = "jdbc:postgresql://dpg-cq5l0kuehbks73bqfs80-a.oregon-postgres.render.com:5432/mypostgres_mrkr";
+    private static final String DB_USERNAME = "admin";
+    private static final String DB_PASSWORD = "9kw0FT61QuWRsVpVq0v0XIj23WkHJCPE";
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
